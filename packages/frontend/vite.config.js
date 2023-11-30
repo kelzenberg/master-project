d0517ea62a59c21922b747a479d6d6e4a2eeebee
Frederik Busch <frederikbusch@MacBook-Air-von-Frederik.local>
2023-11-30 19:19:32 +0100

    removed vite plugin for easier testing of changes

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,9 +1,7 @@
-import eslint from 'vite-plugin-eslint';
-
 /** @type {import('vite').UserConfig} */
 export default {
   root: './src',
   envDir: '../',
   build: { outDir: '../dist', emptyOutDir: true },
-  plugins: [eslint({ lintOnStart: true })],
+  plugins: [],
 };
