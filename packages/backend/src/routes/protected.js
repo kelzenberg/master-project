7fb4ec243b5d94620d7416910d9d904506760561
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-12-03 15:44:59 +0100

    rename public and protected folders

--- packages/backend/src/routes/protected.js
+++ packages/backend/src/routes/protected.js
@@ -1,4 +1,4 @@
 import express from 'express';
-import { handler as protectedTestHandler } from '../handlers/protected-test/protected.js';
+import { handler as protectedTestHandler } from '../handlers/protected/protected.js';
 
 export const routes = express.Router().get('/protected', protectedTestHandler);
--- packages/backend/src/routes/public.js
+++ packages/backend/src/routes/public.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { handler as livenessHandler } from '../handlers/health/liveness.js';
-import { handler as readinessHandler } from '../handlers/health/readiness.js';
+import { handler as livenessHandler } from '../handlers/public/liveness.js';
+import { handler as readinessHandler } from '../handlers/public/readiness.js';
 
 export const routes = express.Router().get('/liveness', livenessHandler).get('/readiness', readinessHandler);
