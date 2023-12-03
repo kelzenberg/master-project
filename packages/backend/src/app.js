56c66e7edea18e3481a334ce56004a24412ec43f
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2023-12-03 15:49:24 +0100

    serve static files

--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -1,3 +1,5 @@
+import path from 'node:path';
+import url from 'node:url';
 import express from 'express';
 import bodyParser from 'body-parser';
 import { authorizer } from './middlewares/authorizer.js';
@@ -9,6 +11,8 @@ export const createApp = logger => {
   const app = express().disable('x-powered-by');
 
   app.use(bodyParser.json());
+  app.use('/', express.static(path.join(path.dirname(url.fileURLToPath(import.meta.url)), 'static')));
+  app.get('/favicon.ico', (req, res) => res.status(204).end()); // temporarily until favicon is available
   app.use(publicRoutes);
   app.use(authorizer);
   app.use(protectedRoutes);
