dc6de69619563b3ce6dbae846f83ad6feb440160
Marc_w <marc.waclaw97@gmail.com>
2024-01-07 09:59:21 +0100

    added backbtn functionaltiy

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -1,2 +1,5 @@
 const previewElements = document.querySelectorAll('simulation-preview');
 for (const element of previewElements) element.setAttribute('href', './simulation.html');
+
+const previewSelection = document.querySelectorAll('menu-preview');
+for (const element of previewSelection) element.setAttribute('href', './index.html');
