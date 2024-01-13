ce2ccf3d9e7722f41c414cc9c9cc8902b9cf6d81
fredbusch98 <frederik.bln98@gmail.com>
2024-01-13 15:30:54 +0100

    added basic implementation of SliderController

--- packages/frontend/src/js/controllers/slider-controller.js
+++ packages/frontend/src/js/controllers/slider-controller.js
@@ -1,1 +1,25 @@
-export class SliderController {}
+export class SliderController {
+  #sliders;
+
+  constructor(sliders) {
+    this.#sliders = sliders;
+  }
+
+  initializeSliders() {
+    const sliderContainer = document.querySelector('#sliderContainer');
+
+    for (const sliderData of this.#sliders) {
+      const rangeSlider = document.createElement('range-slider');
+
+      // Setzen der Attribute für den Range-Slider
+      rangeSlider.setAttribute('min', sliderData.min);
+      rangeSlider.setAttribute('max', sliderData.max);
+      rangeSlider.setAttribute('value', sliderData.default);
+      rangeSlider.setAttribute('label', sliderData.label);
+      rangeSlider.setAttribute('scale', sliderData.scale);
+      rangeSlider.setAttribute('info', sliderData.info);
+
+      sliderContainer.append(rangeSlider);
+    }
+  }
+}
