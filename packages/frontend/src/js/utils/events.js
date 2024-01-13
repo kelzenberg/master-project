3e5748188b92016cfd1866f7b8c3d1f5882e01b9
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-13 15:43:01 +0100

    send initial JSON config to frontend on connection

--- packages/frontend/src/js/utils/events.js
+++ packages/frontend/src/js/utils/events.js
@@ -3,6 +3,10 @@ import { SocketEventTypes } from '@master-project/libs/src/events';
 // eslint-disable-next-line no-undef
 const socket = io(); // `io` object is being exported by '/socket.io/socket.io.js'
 
+socket.on(SocketEventTypes.INIT, message => {
+  console.log(`[TEST]: Socket event on ${SocketEventTypes.INIT} arrived with payload: ${JSON.stringify(message)}`);
+});
+
 socket.on(SocketEventTypes.MESSAGE, message => {
   console.log(`[TEST]: Socket event on ${SocketEventTypes.MESSAGE} arrived with payload: ${message}`);
 });
