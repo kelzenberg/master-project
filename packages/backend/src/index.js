37ae6ea4f9b551fc14b1002e257daa4ca48ef7a6
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-15 15:44:48 +0100

    rename websockets file in backend

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -2,7 +2,7 @@ import bluebird from 'bluebird';
 import stoppable from 'stoppable';
 import { createServer } from 'node:http';
 import { createApp } from './app.js';
-import { startSocketServer } from './websockets.js';
+import { startSocketServer } from './sockets.js';
 import { Logger } from './utils/logger.js';
 
 const logger = Logger({ name: 'server' });
