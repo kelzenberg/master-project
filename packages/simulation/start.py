080d750cf89aa38bbb2f6d5f942f09d728c54758
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-16 17:02:56 +0100

    fix Python Flask server within docker

--- packages/simulation/start.py
+++ packages/simulation/start.py
@@ -361,6 +361,7 @@ if __name__ == "__main__":
         return jsonify(success=False)
 
     try:
-        app.run(host="127.0.0.1")
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port)
     except Exception as exc:
         print(exc)
