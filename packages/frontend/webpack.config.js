31576dd0108f73fcd532dc21f481810ed7f6355f
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-18 20:01:34 +0100

    WIP: import plotly differently

--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -2,7 +2,7 @@
 /* eslint-disable unicorn/prefer-module */
 const path = require('node:path');
 const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 
 module.exports = {
   mode: 'production',
@@ -12,7 +12,7 @@ module.exports = {
     clean: true,
   },
   plugins: [
-    new webpack.debug.ProfilingPlugin(),
+    // new webpack.debug.ProfilingPlugin(),
     new HtmlBundlerPlugin({
       entry: { index: './src/index.html', simulation: './src/simulation.html' },
       js: { filename: 'js/[name].[contenthash:8].js' },
@@ -20,6 +20,7 @@ module.exports = {
     }),
   ],
   module: {
+    noParse: /plotly/,
     rules: [
       {
         test: /\.(css|sass|scss)$/,
