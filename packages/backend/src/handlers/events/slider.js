14ccd10cafc1f63b08c680c14ade0a838413e59a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-22 18:11:52 +0100

    finish slider event data propagation to sim server #87

--- packages/backend/src/handlers/events/slider.js
+++ packages/backend/src/handlers/events/slider.js
@@ -1,17 +1,18 @@
 import { SocketEventTypes } from '../../utils/events.js';
 
 export const handler = (logger, url) => async data => {
+  const payload = { name: `${data.label}`, value: `${data.value}` };
   logger.info(
     `Received message on ${SocketEventTypes.SLIDER.toUpperCase()}. Propagating data to simulation server...`,
-    { data }
+    { data: { received: data, payload } }
   );
-  //
+
   try {
-    const response = await fetch(url);
-    logger.info('Response from sim server', { data: response });
-    // ...
+    const response = await fetch(url, { method: 'POST', body: JSON.stringify(payload) });
+    const responseData = await response.json();
+    logger.info('Response from simulation server', { data: responseData });
   } catch (error) {
-    logger.error('Retrieving initial config failed', error);
-    throw new Error('Retrieving initial config failed', error);
+    logger.error('Sending updated slider values failed', error, { payload });
+    throw new Error('Sending updated slider values failed', error, { payload });
   }
 };
