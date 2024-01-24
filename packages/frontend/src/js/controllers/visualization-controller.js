1b7b3fb224ca0744f15c44dd372f67070d7ceaf5
fredbusch98 <frederik.bln98@gmail.com>
2024-01-24 09:40:00 +0100

    removed commented out code

--- packages/frontend/src/js/controllers/visualization-controller.js
+++ packages/frontend/src/js/controllers/visualization-controller.js
@@ -88,8 +88,6 @@ export class VisualizationController {
   }
 
   animate() {
-    //this.#resizeCanvasToDisplaySize();
-
     requestAnimationFrame(() => this.animate());
     this.#controls.update();
     this.#renderer.render(this.#scene, this.#camera);
