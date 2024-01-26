abb52ef1364ac6c321c1f6f7509c655553d1ab6a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-26 19:38:53 +0100

    grab initial config from Python server and store it in sim config

--- packages/backend/src/config.js
+++ packages/backend/src/config.js
@@ -1,6 +1,9 @@
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import path from 'node:path';
 import { v4 as uuid } from 'uuid';
+import { Logger } from './utils/logger.js';
+
+const logger = Logger({ name: 'config-loader' });
 
 export const loadConfig = async () => {
   const filePath = path.resolve(`${process.env.CONFIG_PATH}` || './config.json');
@@ -18,4 +21,50 @@ export const loadConfig = async () => {
   }
 
   return simulations;
+};
+
+export const fetchMissingData = async () => {
+  const simConfigs = await loadConfig();
+
+  const updatedSimConfigs = await Promise.all(
+    Object.values(simConfigs).map(async value => {
+      const { name, simURL, endpoints } = value;
+
+      try {
+        logger.info(`Requesting to start ${name} sim...`);
+
+        const startResponse = await fetch(`${simURL}${endpoints.start.path}`, { method: endpoints.start.method });
+        // eslint-disable-next-line unicorn/no-await-expression-member
+        const hasStarted = (await startResponse.json()).success;
+
+        if (!hasStarted) {
+          logger.warn(`Python sim ${name} could not be started. Trying to resume it...`, { hasStarted });
+          await fetch(`${simURL}${endpoints.resume.path}`, { method: endpoints.resume.method });
+        }
+
+        logger.info(`Python sim ${name} sim has started`);
+      } catch (error) {
+        logger.error(`Starting Python sim ${name} failed`, error);
+        throw new Error(error);
+      }
+
+      try {
+        logger.info(`Fetching initial data for ${name} sim...`);
+
+        const initialResponse = await fetch(`${simURL}${endpoints.initial.path}`, { method: endpoints.initial.method });
+        value.data = { initial: await initialResponse.json() };
+
+        logger.info(`Stored initial config for ${name} sim`);
+      } catch (error) {
+        logger.error(`Retrieving initial config for ${name} sim failed`, error);
+        throw new Error(error);
+      }
+
+      return value;
+    })
+  );
+
+  await writeFile('./test_config.local.json', JSON.stringify(updatedSimConfigs), { encoding: 'utf8' });
+
+  return updatedSimConfigs;
 };
--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -5,60 +5,24 @@ import { createApp } from './app.js';
 import { startSocketServer } from './sockets.js';
 import { FetchWorker } from './models/Worker.js';
 import { Logger } from './utils/logger.js';
+import { fetchMissingData } from './config.js';
 
 const logger = Logger({ name: 'server' });
 const expressPort = process.env.BACKEND_PORT || 3000;
 const simServerURL = `http://${process.env.SIMULATION_URL}:${process.env.SIMULATION_PORT}`;
 const startURL = `${simServerURL}/start`;
+const resumeURL = `${simServerURL}/resume`;
 const staticURL = `${simServerURL}/initial`;
 const dynamicURL = `${simServerURL}/dynamic`;
 const sliderURL = `${simServerURL}/slider`;
+
+const data = await fetchMissingData();
 
 // ExpressJS
 const expressServer = createServer(createApp(logger));
 const stoppableServer = stoppable(
   expressServer.listen(expressPort, async () => {
     logger.info(`Server started on port ${expressPort}.`);
-
-    // ToDo: Temporary sim list until sim config file is present
-    const simList = {
-      1234: {
-        id: 1234,
-        name: 'Methanation',
-        description: 'This is the sim for Methanation...',
-        initial: null,
-      },
-    };
-
-    // ToDo: this is placed temporarily here until the backend knows all simulations, their URLs
-    // and has an option to dynamically get and store the initial sim data
-    try {
-      logger.info(`Fetching initial config for sim ${1234}...`);
-      const startResponse = await fetch(startURL, { method: 'POST' });
-      // eslint-disable-next-line unicorn/no-await-expression-member
-      const hasStarted = (await startResponse.json()).success;
-
-      if (!hasStarted) {
-        const message = 'Python sim could not be started';
-        logger.error(message, { hasStarted });
-        throw new Error(message);
-      }
-
-      logger.info('Python sim has started');
-    } catch (error) {
-      logger.error(error);
-      throw new Error(error);
-    }
-
-    try {
-      logger.info(`Fetching initial data...`);
-      const response = await fetch(staticURL);
-      simList[1234].initial = await response.json();
-      logger.info(`Stored initial config for sim ${1234}`);
-    } catch (error) {
-      logger.error('Retrieving initial config failed', error);
-      throw new Error('Retrieving initial config failed', error);
-    }
 
     try {
       // NodeJS Workers
