6688b5b268b8293d061f9c80b13dfae36ad41a8a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-27 20:12:26 +0100

    make name and description on simulation model public

--- packages/backend/src/models/Simulation.js
+++ packages/backend/src/models/Simulation.js
@@ -5,31 +5,31 @@ import { Logger } from '../utils/logger';
  * Represents all information of a Python simulation in the cluster
  */
 export class Simulation {
-  #name;
-  #description;
   #URL;
   #data;
   #worker;
   #logger;
 
+  name;
+  description;
   isHealthy;
 
   constructor({ name, description, simURL }) {
-    this.#name = name;
-    this.#description = description;
+    this.name = name;
+    this.description = description;
     this.#URL = simURL;
     this.#data = { initial: null };
-    this.#logger = new Logger({ name: `simulation-instance-${this.#name}` });
+    this.#logger = new Logger({ name: `simulation-instance-${this.name}` });
     this.isHealthy = null;
   }
 
   async #getSimHealth() {
-    this.#logger.info(`Checking if ${this.#name} Python sim is healthy...`);
+    this.#logger.info(`Checking if ${this.name} Python sim is healthy...`);
     const response = await fetch(`${this.#URL}/start`, { method: 'POST' });
     const { success: isHealthy } = await response.json();
 
     this.isHealthy = isHealthy;
-    this.#logger.info(`Python sim ${this.#name} is ${isHealthy ? 'healthy' : 'unhealthy'}...`);
+    this.#logger.info(`Python sim ${this.name} is ${isHealthy ? 'healthy' : 'unhealthy'}...`);
     return isHealthy;
   }
 
@@ -48,46 +48,46 @@ export class Simulation {
     };
 
     const isHealthy = check();
-    this.#logger[isHealthy ? 'info' : 'error'](`Python sim ${this.#name} is ${isHealthy ? 'alive' : 'not available'}`);
+    this.#logger[isHealthy ? 'info' : 'error'](`Python sim ${this.name} is ${isHealthy ? 'alive' : 'not available'}`);
   }
 
   async start() {
-    this.#logger.info(`Requesting to start ${this.#name} Python sim...`);
+    this.#logger.info(`Requesting to start ${this.name} Python sim...`);
 
     try {
       const response = await fetch(`${this.#URL}/start`, { method: 'POST' });
       const { success: hasStarted } = await response.json();
 
       if (hasStarted) {
-        this.#logger.info(`Python sim ${this.#name} has started`);
+        this.#logger.info(`Python sim ${this.name} has started`);
         return true;
       }
 
-      this.#logger.warn(`Python sim ${this.#name} could not be started. Trying to reset it...`);
+      this.#logger.warn(`Python sim ${this.name} could not be started. Trying to reset it...`);
       const hasReset = this.reset();
       return hasReset;
     } catch (error) {
-      this.#logger.error(`Starting Python sim ${this.#name} failed`, error);
+      this.#logger.error(`Starting Python sim ${this.name} failed`, error);
     }
 
     return false;
   }
 
   async reset() {
-    this.#logger.info(`Requesting to reset ${this.#name} Python sim...`);
+    this.#logger.info(`Requesting to reset ${this.name} Python sim...`);
 
     try {
       const response = await fetch(`${this.#URL}/reset`, { method: 'POST' });
       const { success: hasReset } = await response.json();
 
       if (hasReset) {
-        this.#logger.info(`Python sim ${this.#name} has reset`);
+        this.#logger.info(`Python sim ${this.name} has reset`);
         return true;
       }
 
-      this.#logger.error(`Python sim ${this.#name} could not be reset...`);
+      this.#logger.error(`Python sim ${this.name} could not be reset...`);
     } catch (error) {
-      this.#logger.error(`Resetting Python sim ${this.#name} failed`, error);
+      this.#logger.error(`Resetting Python sim ${this.name} failed`, error);
     }
 
     return false;
