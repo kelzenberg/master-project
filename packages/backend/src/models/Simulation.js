b1a95e5d56fbe798406c801f22c5ad0eeb7dddb8
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-28 00:35:35 +0100

    rename sim config.name to title

--- packages/backend/src/models/Simulation.js
+++ packages/backend/src/models/Simulation.js
@@ -14,37 +14,37 @@ export class Simulation {
   #logger;
 
   id;
-  name;
+  title;
   description;
   createdAt;
 
-  constructor({ name, description, envKeyForURL }) {
+  constructor({ title, description, envKeyForURL }) {
     this.id = uuid();
-    this.name = name;
+    this.title = title;
     this.description = description;
     this.createdAt = new Date().toISOString();
     this.#URL = `http://${process.env[envKeyForURL + '_URL']}:${process.env.SIMULATION_PORT}`;
     this.#isHealthy = null;
     this.#data = { initial: null };
-    this.#fetchWorker = new FetchWorker(this.name, `${this.#URL}/dynamic`);
-    this.#logger = Logger({ name: `simulation-instance-${this.name}` });
+    this.#fetchWorker = new FetchWorker(this.title, `${this.#URL}/dynamic`);
+    this.#logger = Logger({ name: `simulation-instance-${this.title}` });
   }
 
   async #getSimHealth(attempt = 1) {
-    this.#logger.info(`Checking health of ${this.name} Python sim (attempt #${attempt})...`);
+    this.#logger.info(`Checking health of ${this.title} Python sim (attempt #${attempt})...`);
 
     try {
       const response = await fetch(`${this.#URL}/health`, { method: 'GET' });
       const { success: isHealthy } = await response.json();
       this.#isHealthy = isHealthy;
     } catch (error) {
-      const message = `Checking health for ${this.name} sim failed`;
+      const message = `Checking health for ${this.title} sim failed`;
       this.#logger.error(message, error);
       throw new Error(message, error);
     }
 
     this.#logger[this.#isHealthy ? 'info' : 'warn'](
-      `Python sim ${this.name} is ${this.#isHealthy ? 'healthy' : 'UNHEALTHY'}`
+      `Python sim ${this.title} is ${this.#isHealthy ? 'healthy' : 'UNHEALTHY'}`
     );
     return this.#isHealthy;
   }
@@ -63,11 +63,11 @@ export class Simulation {
       return await checkHealth();
     };
 
-    this.#logger.info(`Waiting for Python sim ${this.name} to become ready to accept connections...`);
+    this.#logger.info(`Waiting for Python sim ${this.title} to become ready to accept connections...`);
     const isHealthy = await checkHealth();
 
     if (!isHealthy) {
-      const message = `Python sim ${this.name} is not responding healthy after ${repeatCounter} attempts. Aborting...`;
+      const message = `Python sim ${this.title} is not responding healthy after ${repeatCounter} attempts. Aborting...`;
       this.#logger.error(message);
       throw new Error(message);
     }
@@ -75,56 +75,56 @@ export class Simulation {
 
   async fetchInitialData() {
     try {
-      this.#logger.info(`Fetching initial data for ${this.name} sim...`);
+      this.#logger.info(`Fetching initial data for ${this.title} sim...`);
 
       const response = await fetch(`${this.#URL}/initial`, { method: 'GET' });
       this.#data = { initial: await response.json() };
 
-      this.#logger.info(`Stored initial data for ${this.name} sim`);
+      this.#logger.info(`Stored initial data for ${this.title} sim`);
       return this.#data.initial;
     } catch (error) {
-      this.#logger.error(`Retrieving initial data for ${this.name} sim failed`, error);
+      this.#logger.error(`Retrieving initial data for ${this.title} sim failed`, error);
       throw new Error(error);
     }
   }
 
   async start() {
-    this.#logger.info(`Requesting to start ${this.name} Python sim...`);
+    this.#logger.info(`Requesting to start ${this.title} Python sim...`);
 
     try {
       const response = await fetch(`${this.#URL}/start`, { method: 'POST' });
       const { success: hasStarted } = await response.json();
 
       if (hasStarted) {
-        this.#logger.info(`Python sim ${this.name} has started`);
+        this.#logger.info(`Python sim ${this.title} has started`);
         return true;
       }
 
-      this.#logger.warn(`Python sim ${this.name} could not be started. Trying to reset it...`);
+      this.#logger.warn(`Python sim ${this.title} could not be started. Trying to reset it...`);
       const hasReset = this.reset();
       return hasReset;
     } catch (error) {
-      this.#logger.error(`Starting Python sim ${this.name} failed`, error);
+      this.#logger.error(`Starting Python sim ${this.title} failed`, error);
     }
 
     return false;
   }
 
   async reset() {
-    this.#logger.info(`Requesting to reset ${this.name} Python sim...`);
+    this.#logger.info(`Requesting to reset ${this.title} Python sim...`);
 
     try {
       const response = await fetch(`${this.#URL}/reset`, { method: 'POST' });
       const { success: hasReset } = await response.json();
 
       if (hasReset) {
-        this.#logger.info(`Python sim ${this.name} has reset`);
+        this.#logger.info(`Python sim ${this.title} has reset`);
         return true;
       }
 
-      this.#logger.error(`Python sim ${this.name} could not be reset...`);
+      this.#logger.error(`Python sim ${this.title} could not be reset...`);
     } catch (error) {
-      this.#logger.error(`Resetting Python sim ${this.name} failed`, error);
+      this.#logger.error(`Resetting Python sim ${this.title} failed`, error);
     }
 
     return false;
