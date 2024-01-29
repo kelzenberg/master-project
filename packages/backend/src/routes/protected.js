f4a6f254bdf7b9174004ea22a372a7a903e8977f
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-29 19:08:02 +0100

    move /list endpoint into unprotected

--- packages/backend/src/routes/protected.js
+++ packages/backend/src/routes/protected.js
@@ -1,5 +1,4 @@
 import express from 'express';
 import { handler as protectedTestHandler } from '../handlers/protected/protected.js';
-import { handler as simListHandler } from '../handlers/protected/sim-list.js';
 
-export const routes = express.Router().get('/protected', protectedTestHandler).get('/list', simListHandler);
+export const routes = express.Router().get('/protected', protectedTestHandler);
