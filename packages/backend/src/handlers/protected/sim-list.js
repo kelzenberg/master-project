f9f95e01a72bb5f0c5774ef7f1987f2c0e85d87f
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-01-29 19:19:35 +0100

    improve sim-list /list endpoint

--- packages/backend/src/handlers/protected/sim-list.js
+++ packages/backend/src/handlers/protected/sim-list.js
@@ -3,15 +3,29 @@ import { simControllers } from '../../index.js';
 export const handler = async (req, res) => {
   const querySimId = req.query.id;
 
+  // Get details from one simulation only
   if (querySimId) {
-    // Get details from one simulation only
     const simController = simControllers.find(sim => sim.id === querySimId);
-    res
-      .status(200)
-      .send({ id: simController.id, description: simController.description, thumbnail: simController.getThumbnail() });
-  } else {
-    // Send all simulation IDs
-    const simIds = simControllers.map(sim => sim.id);
-    res.status(200).send(simIds);
+
+    if (!simController) {
+      res.status(401).send(`Simulation with ID ${querySimId} could not be found`);
+      return;
+    }
+
+    res.status(200).send({
+      title: simController.title,
+      description: simController.description,
+    });
+    return;
   }
+
+  // Send all simulation IDs
+  const simIds = simControllers.map(simController => ({
+    id: simController.id,
+    title: simController.title,
+    description: simController.description,
+    thumbnail: simController.getThumbnail(),
+  }));
+
+  res.status(200).send(simIds);
 };
