5eec36a2df04e97e5e6ea2a08fd3aaa37df750e7
Leonardin96 <lennartreinke@yahoo.de>
2024-01-31 17:48:24 +0100

    Range slider rework

--- packages/frontend/src/js/components/range-slider.js
+++ packages/frontend/src/js/components/range-slider.js
@@ -5,39 +5,77 @@ template.innerHTML = `
           <span id="rangeSliderLabel"></span>
           <div id="rangeSliderIcon">
             <div class="rangeSliderModal">
+              <span class="modalContent" id="rangerSliderLogInfo">logarithmic scale</span>
               <span class="modalContent" id="infoMin"></span>
               <span class="modalContent" id="infoMax"></span>
+              <span id="rangeSliderInfo"></span>
             </div>
           </div>
         </div>
-        <input type="range" min="1" max="100" value="50" id="slider">
+        <input type="range" min="0" max="100" value="50" step="1" id="slider">
         <span id="rangeSliderValue"></span>
     </div>
 `;
 
 class rangeSlider extends HTMLElement {
   static get observedAttributes() {
-    return ['min', 'max', 'label', 'value', 'scale', 'disabled'];
+    return ['min', 'max', 'label', 'value', 'scale', 'disabled', 'info', 'islogscale'];
   }
 
   constructor() {
     super();
 
+    // Attribute values
     this.labelText = '';
-    this.min = 1;
+    this.infoText = '';
+    this.min = 0;
     this.max = 100;
     this.value = 50;
     this.disabled = false;
 
+    // other variables
+    this.steps = 100;
+    this.stepSize = 1;
+    this.isLogScale = '';
+    this.initialMin = 0;
+    this.initialMax = 0;
+
     this.inputHandler = this.handleInput.bind(this);
+  }
+
+  calculateStepSize() {
+    this.stepSize = (this.max - this.min) / this.steps;
+  }
+
+  calculateSymmetricalLog(value) {
+    // handle 0 directly
+    if (Number(value) === 0) return value;
+
+    const absLogValue = Math.log10(Math.abs(value));
+
+    return value > 0 ? absLogValue : -absLogValue;
+  }
+
+  sliderPositionToValue(position) {
+    if (!this.isLogScale) {
+      return Number(this.min) + this.stepSize * position;
+    }
+
+    const logValue = Number(this.min) + this.stepSize * position;
+
+    return Math.pow(10, logValue);
+  }
+
+  sliderPosition(value) {
+    return (Number(value) - Number(this.min)) / this.stepSize;
   }
 
   handleInput(event) {
     if (!event.target.value) return;
 
-    const value = event.target.value * 1;
+    const value = this.sliderPositionToValue(event.target.value);
 
-    if (value <= this.min || value >= this.max) return;
+    if (value < this.initialMin || value > this.initialMax) return;
 
     this.value = value;
 
@@ -56,19 +94,24 @@ class rangeSlider extends HTMLElement {
   renderValues() {
     if (!this.text) return;
 
+    this.info.textContent = this.infoText;
     this.label.textContent = this.labelText;
-    this.text.textContent = this.value;
+    this.text.textContent = Number.parseFloat(this.value).toPrecision(3);
     // eslint-disable-next-line unicorn/prefer-dom-node-text-content
-    this.modalInfoMin.innerText = 'Minimum: ' + this.min;
+    this.modalInfoMin.innerText = 'Minimum: ' + this.initialMin;
     // eslint-disable-next-line unicorn/prefer-dom-node-text-content
-    this.modalInfoMax.innerText = 'Maximum: ' + this.max;
+    this.modalInfoMax.innerText = 'Maximum: ' + this.initialMax;
   }
 
   setAttributeValues() {
-    this.input?.setAttribute('min', this.min);
-    this.input?.setAttribute('max', this.max);
-    this.input?.setAttribute('value', this.value);
+    this.input?.setAttribute('value', this.sliderPosition(this.value));
     this.input?.toggleAttribute('disabled', this.disabled === true || this.disabled === 'true');
+  }
+
+  convertStringToBool(value) {
+    if (typeof value !== 'string') return value;
+
+    return value === 'true';
   }
 
   // Life-cycle methods
@@ -81,11 +124,15 @@ class rangeSlider extends HTMLElement {
         break;
       }
       case 'min': {
-        this.min = newValue;
+        this.initialMin = newValue;
+        this.min = this.isLogScale ? this.calculateSymmetricalLog(newValue) : newValue;
+        this.calculateStepSize();
         break;
       }
       case 'max': {
-        this.max = newValue;
+        this.initialMax = newValue;
+        this.max = this.isLogScale ? this.calculateSymmetricalLog(newValue) : newValue;
+        this.calculateStepSize();
         break;
       }
       case 'value': {
@@ -94,6 +141,15 @@ class rangeSlider extends HTMLElement {
       }
       case 'disabled': {
         this.disabled = newValue;
+        break;
+      }
+      case 'info': {
+        this.info = newValue;
+        break;
+      }
+      case 'islogscale': {
+        this.isLogScale = this.convertStringToBool(newValue);
+        if (this.modalLogInfo) this.modalLogInfo.style.display = this.isLogScale ? 'block' : 'none';
         break;
       }
     }
@@ -108,7 +164,9 @@ class rangeSlider extends HTMLElement {
     this.querySelector('#slider').addEventListener('input', this.inputHandler);
 
     this.label = this.querySelector('#rangeSliderLabel');
+    this.info = this.querySelector('#rangeSliderInfo');
     this.text = this.querySelector('#rangeSliderValue');
+    this.modalLogInfo = this.querySelector('#rangerSliderLogInfo');
     this.modalInfoMin = this.querySelector('#infoMin');
     this.modalInfoMax = this.querySelector('#infoMax');
     this.input = this.querySelector('input');
--- packages/frontend/src/js/controllers/LegendController.js
+++ packages/frontend/src/js/controllers/LegendController.js
@@ -29,6 +29,7 @@ export class LegendController {
         circleDiv.style.borderRadius = '50%';
         circleDiv.style.width = `${type.radius * 15}px`; // Adjust the size as needed
         circleDiv.style.height = `${type.radius * 15}px`; // Adjust the size as needed
+        circleDiv.style.margin = 'auto';
         circleCell.append(circleDiv);
 
         // Create a table data for the name
