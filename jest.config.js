a148dcd5c01502100219d5e1a5e63e73a2f87eec
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-02-05 16:14:01 +0100

    use babel in jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
+const esModules = ['plotly.js-dist-min'].join('|');
+
 /** @type {import('jest').Config} */
 const config = {
   rootDir: '.',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
   clearMocks: true,
+  transform: {
+    '^.+\\.(m?js|ts)$': 'babel-jest', // transpile mjs, mts, js, ts files
+  },
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+  verbose: true,
 };
 
 // eslint-disable-next-line unicorn/prefer-module, no-undef
--- setup-jest.js
+++ setup-jest.js
@@ -1,10 +1,6 @@
-/* eslint-disable no-undef */
+import { jest, beforeEach, afterAll } from '@jest/globals';
 
-<<<<<<< Updated upstream
 jest.mock('<rootDir>/packages/frontend/src/js/services/sockets');
-=======
-jest.mock('<rootDir>/packages/frontend/src/js/services/sockets.js');
->>>>>>> Stashed changes
 
 beforeEach(() => {
   jest.clearAllMocks();
