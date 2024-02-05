4cfc3b20d70bd77406ce6b345c4b331b5f73ec47
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-02-05 19:37:13 +0100

    fix calling jest tests with scope

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,16 @@ const esModules = ['plotly.js-dist-min', 'three'].join('|');
 // eslint-disable-next-line unicorn/prefer-module, no-undef
 module.exports = {
   rootDir: '.',
+  testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
-  clearMocks: true,
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  modulePathIgnorePatterns: ['dist'],
+  moduleNameMapper: {
+    '@master-project/libs/(.*)$': '<rootDir>/packages/libs/src',
+  },
   transform: {
     '^.+\\.(m?js|ts)$': ['babel-jest', { rootMode: 'upward' }], // transpile mjs, mts, js, ts files
   },
   transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
-  testEnvironment: 'node',
   verbose: false, // for debugging only
 };
