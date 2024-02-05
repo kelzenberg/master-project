a324c3a2d682b37979180428f51a47dfc5def468
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-02-05 15:17:23 +0100

    add setup-jest script

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.js'],
+};
+
+// eslint-disable-next-line unicorn/prefer-module, no-undef
+module.exports = config;
---
+++ setup-jest.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+
+jest.mock('./packages/frontend/src/js/services/sockets.js');
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+afterAll(() => {});
