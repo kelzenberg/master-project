f4604cc15c4cf6431af882f747c671d6cb17015a
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-02-06 17:13:30 +0100

    add script to generate local certs for HTTPS

---
+++ packages/backend/generate-local-cert.sh
@@ -0,0 +1,15 @@
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+folder_path="$script_dir/src/cert2"
+
+generate_local_certs() {
+  echo "Generating local self-signed certificate files in '$folder_path'.\nPlease follow instructions.\n"
+
+  mkdir -p $folder_path
+  openssl genrsa 1024 >$folder_path/file.pem
+  openssl req -new -key $folder_path/file.pem -out $folder_path/csr.pem
+  openssl x509 -req -days 365 -in $folder_path/csr.pem -signkey $folder_path/file.pem -out $folder_path/file.crt
+
+  echo "\nLocal self-signed certificate files generated. DO NOT USE THESE IN PRODUCTION!"
+}
+
+generate_local_certs
