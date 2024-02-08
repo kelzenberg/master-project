5ab32252700934169b014a7ab95aeb16e1297ec3
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-02-08 16:58:20 +0100

    add root jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+/* eslint-disable unicorn/prefer-module */
+/** @type {import('jest').Config} */
+module.exports = {
+  rootDir: './',
+  projects: ['<rootDir>/packages/backend/jest.config.js'],
+};
