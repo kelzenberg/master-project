912b9d127c1dfc68c3913ebb82fc043de4b12d2c
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-02-08 18:46:33 +0100

    sort app.js imports

--- packages/backend/src/app.js
+++ packages/backend/src/app.js
@@ -7,10 +7,10 @@ import helmet from 'helmet';
 import basicAuth from 'express-basic-auth';
 import favicon from 'serve-favicon';
 import { authorizer } from './middlewares/authorizer.js';
+import { redirectHandler } from './middlewares/redirect-handler.js';
+import { createErrorHandler } from './middlewares/error-handler.js';
 import { routes as publicRoutes } from './routes/public.js';
 import { routes as protectedRoutes } from './routes/protected.js';
-import { createErrorHandler } from './middlewares/error-handler.js';
-import { redirectHandler } from './middlewares/redirect-handler.js';
 
 export const createApp = logger => {
   const app = express().disable('x-powered-by');
