defc91765206634e6f6c8a01e281c1f1da3fd074
Steffen Ansorge <kelzenberg@users.noreply.github.com>
2024-02-09 19:08:01 +0100

    fix FE dev server

--- packages/frontend/webpack.dev.config.js
+++ packages/frontend/webpack.dev.config.js
@@ -1,16 +1,15 @@
-/* eslint-disable no-undef */
-/* eslint-disable unicorn/prefer-module */
-const path = require('node:path');
-const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+import path from 'node:path';
+import url from 'node:url';
+import HtmlBundlerPlugin from 'html-bundler-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 
-module.exports = {
+export default {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    static: path.join(__dirname, './dist/'),
+    static: path.join(path.dirname(url.fileURLToPath(import.meta.url)), './dist/'),
     hot: true,
-    port: 3000,
+    port: 3080,
     watchFiles: {
       paths: ['src/**/*.*'],
       options: {
@@ -22,7 +21,7 @@ module.exports = {
     runtimeChunk: 'single',
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), 'dist'),
     clean: true,
   },
   plugins: [
